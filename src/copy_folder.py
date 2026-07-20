406125b08d1fa03d7de1738c6a28b4f2608ea9de
emreh773 <emrezh@gmail.com>
2026-07-20 17:48:25 +0300

    Added file generation

---
+++ src/copy_folder.py
@@ -0,0 +1,45 @@
+import shutil
+from pathlib import Path
+
+def clear_destination(destination_root: Path) -> None:
+    if destination_root == Path(".") or destination_root == Path("/"):
+        raise ValueError("Don't delete this please")
+    
+    if destination_root.exists():
+        shutil.rmtree(destination_root)
+        print(f"Cleared: {destination_root}")
+
+    destination_root.mkdir(parents=True)
+    print(f"Created empty: {destination_root}")
+
+def copy_item(source: Path, destination_root: Path, relative_to: Path) -> None:
+    relative_path = source.relative_to(relative_to)
+    target = destination_root / relative_path
+
+    if source.is_dir():
+        target.mkdir(parents=True, exist_ok=True)
+        print(f"Created dir:  {target}")
+    else:
+        target.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy2(source, target)
+        print(f"Copied file:  {source} -> {target}")
+
+
+def scan_folder(source: Path, destination_root: Path, relative_to: Path | None = None) -> None:
+    if relative_to is None:
+        relative_to = source
+        clear_destination(destination_root)
+
+    for entry in source.iterdir():
+        copy_item(entry, destination_root, relative_to)
+
+        if entry.is_dir():
+            scan_folder(entry, destination_root, relative_to)
+
+
+if __name__ == "__main__":
+    source = Path("source_folder")
+    destination = Path("destination_folder")
+
+    clear_destination(destination)
+    scan_folder(source, destination)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from textnode import TextNode, TextType
+from copy_folder import scan_folder
+from pathlib import Path
 
 print("hello world")
 
 def main():
-    asdf =  TextNode("Hello", TextType.PLAIN, "http://example.com")
+    asdf =  TextNode("Hello", TextType.TEXT, "http://example.com")
     print(asdf)
+    scan_folder(Path("static"), Path("public"))
 
 main()
